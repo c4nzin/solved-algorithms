e142f9ba985d0b3bc8cbc120e4a553bfeac36cda
CANMERTINYO <38213551+burhancan-stack@users.noreply.github.com>
2022-11-30 21:27:31 +0300

    Create convert-string-to-a-number.js

---
+++ convert-string-to-a-number.js
@@ -0,0 +1,10 @@
+const stringToNumber = function(str){
+  // put your code here
+  return Number(str)
+}
+
+stringToNumber("123")
+stringToNumber("123")
+stringToNumber("12123123313")
+stringToNumber("1212313")
+stringToNumber("1211113")

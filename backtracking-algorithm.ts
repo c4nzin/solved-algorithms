2e8f4d12db7196355cb160610b2a66dbcbdb8c21
CANMERTINYO <38213551+canmertinyo@users.noreply.github.com>
2023-09-29 21:03:58 +0300

    Create backtracking-algorithm.ts

---
+++ backtracking-algorithm.ts
@@ -0,0 +1,20 @@
+function generateParanthesis(n: number) {
+  const collectedParanthesis: string[] = [];
+
+  function backtrack(combination = "", openCount = 0, closeCount = 0) {
+    if (combination.length === 2 * n) collectedParanthesis.push(combination);
+
+    if (openCount < n) {
+      backtrack(combination + "(", openCount + 1, closeCount);
+    }
+
+    if (closeCount < openCount) {
+      backtrack(combination + ")", openCount, closeCount + 1);
+    }
+  }
+
+  backtrack();
+
+  return collectedParanthesis;
+}
+
